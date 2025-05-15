e5aaea3bc78645d783dd534b7c3f84147529f0b9
IHaveNoIdeaAboutMyUsername <2233528015@qq.com>
2025-05-15 21:21:54 +0800

    改改

--- Luogu/P11229/P11229.cpp
+++ Luogu/P11229/P11229.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
 using namespace std;
-int n;
-int nums[10] = { 6,2,5,5,4,5,6,3,7,6 };
-int main() {
-	int T;
-	cin >> T;
-	while (T--) {
-		cin >> n;
-
+typedef long long ll;
+const ll inf = 2e9, f[10] = { 6,2,5,5,4,5,6,3,7,6 };
+ll i, ans;
+void dfs(ll gz, ll x) {
+	if (gz > i)return;
+	if (gz == i)return ans = min(ans, x), void();
+	for (ll i = 0; i <= 9; i++) {
+		if (i == 0 && gz == 0)continue;
+		dfs(gz + f[i], x * 10 + i);
 	}
-	return 0;
+}
+int main() {
+	for (i = 1; i <= 40; i++) {
+		ans = inf;
+		dfs(0, 0);
+		cout << i << "--" << ans << '\n';
+	}
 }

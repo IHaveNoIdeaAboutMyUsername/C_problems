9adbb2743d4b9171b808cd93e3ea7284801787c4
IHaveNoIdeaAboutMyUsername <2233528015@qq.com>
2025-05-16 00:21:17 +0800

    齐活

--- Luogu/P11229/P11229.cpp
+++ Luogu/P11229/P11229.cpp
@@ -1,20 +1,70 @@
 #include<iostream>
 using namespace std;
-typedef long long ll;
-const ll inf = 2e9, f[10] = { 6,2,5,5,4,5,6,3,7,6 };
-ll i, ans;
-void dfs(ll gz, ll x) {
-	if (gz > i)return;
-	if (gz == i)return ans = min(ans, x), void();
-	for (ll i = 0; i <= 9; i++) {
-		if (i == 0 && gz == 0)continue;
-		dfs(gz + f[i], x * 10 + i);
-	}
-}
 int main() {
-	for (i = 1; i <= 40; i++) {
-		ans = inf;
-		dfs(0, 0);
-		cout << i << "--" << ans << '\n';
+	int T;
+	cin >> T;
+	while (T--) {
+		int n;
+		cin >> n;
+		switch (n) {
+		case 1:
+			cout << -1;
+			break;
+		case 2:
+			cout << 1;
+			break;
+		case 3:
+			cout << 7;
+			break;
+		case 4:
+			cout << 4;
+			break;
+		case 5:
+			cout << 2;
+			break;
+		case 6:
+			cout << 6;
+			break;
+		case 7:
+			cout << 8;
+			break;
+		}
+		if (n > 7) {
+			int n7 = n % 7;
+			switch (n7) {
+			case 1:
+				cout << 10;
+				for (int i = 1; i < n / 7; i++) cout << 8;
+				break;
+			case 2:
+				cout << 18;
+				for (int i = 1; i < n / 7; i++) cout << 8;
+				break;
+			case 3:	
+				if (n == 10)cout << 22;
+				else {
+					cout << 200;
+					for (int i = 1; i <= n / 7 - 2; i++)cout << 8;
+				}
+				break;
+			case 4:
+				cout << 20;
+				for (int i = 1; i < n / 7; i++) cout << 8;
+				break;
+			case 5:
+				cout << 28;
+				for (int i = 1; i < n / 7; i++) cout << 8;
+				break;
+			case 6:
+				cout << 68;
+				for (int i = 1; i < n / 7; i++) cout << 8;
+				break;
+			case 0:
+				cout << 88;
+				for (int i = 2; i < n / 7; i++) cout << 8;
+				break;
+			}
+		}
+		cout << endl;
 	}
 }
